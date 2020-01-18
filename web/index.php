f3c3e6cde1d376c71079daf36df842e6c4d5c62a
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-01-17 22:52:31 -0700

    testing

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Jordi's Homepage</title>
+  </head>
+  <body>
+    <h1>Welcome to Jordi's Homepage</h1>
+    <p>hi</p>
+  </body>
+</html>
