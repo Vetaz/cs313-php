8504774812da2ac40fe24296bcafb7eb42710179
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-02-04 15:50:10 -0700

    Tuesday database ex. fix

--- web/week05/temp/index.php
+++ web/week05/temp/index.php
@@ -5,7 +5,7 @@
   $family_members = $db->prepare("SELECT * FROM w5_family_members");
   $family_members->execute();
 
-  while ($fRow = $family_members->getch(PDO::FETCH_ASSOC)) {
+  while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC)) {
     $first_name = $fRow['first_name'];
     $last_name = $fRow['last_name'];
     $relationship_id = $fRow['relationship_id'];
