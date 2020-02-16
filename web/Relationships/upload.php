3665e149dacf0a1f9ed4540ab1c799e7a711e98c
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-02-15 22:22:19 -0700

    changing structure

--- web/Relationships/upload.php
+++ web/Relationships/upload.php
@@ -1,14 +1,15 @@
 <?php
-//require "userRequired.php";
+require "userRequired.php";
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 date_default_timezone_set('MST');
-function alert($msg)
-{
+function alert($msg) {
   echo "<script>alert('$msg');</script>";
 }
 
 if (isset($_FILES["filename"])) {
   $errors = array();
-  $target_dir = "tempUploads/";
+  $target_dir = "/app/web/Relationships/tempUploads/";
   $target_file = $target_dir . "USERNAME" . basename(date('YmdHis') . ".ged");
   $fileType = strtolower(pathinfo($_FILES["filename"]["name"], PATHINFO_EXTENSION));
 
@@ -30,8 +31,7 @@ if (isset($_FILES["filename"])) {
     if (move_uploaded_file($_FILES["filename"]["tmp_name"], $target_file)) {
       echo "The file " . basename($_FILES["filename"]["name"]) . " has been uploaded.<br>";
       $_SESSION['filename'] = $target_file;
-      uploadToDatabase();
-      //header("Location: get-relationship.php");
+      header("Location: uploadDatabase.php");
     } else {
       $errors[] = "Sorry, there was an error uploading your file.<br>Page will redirect to the upload page.";
       alert($errors[0]);
@@ -42,77 +42,4 @@ if (isset($_FILES["filename"])) {
   echo "<p>Something went wrong. Redirecting to upload page.</p>";
   alert($errors);
   //header("Location: index.html");
-}
-
-
-function uploadToDatabase()
-{
-  require 'lib/Gedcom/bootstrap.php';
-
-  $file = $_SESSION['filename'];
-
-  $parser = new \Gedcom\Parser();
-
-  try {
-    $gedcom = $parser->parse($file);
-  } catch (Exception $e) {
-    echo $e->getMessage();
-    exit;
-  }
-
-  $errors = $parser->getErrors();
-
-  if (!empty($errors))
-    echo 'The following parser errors occurred: <pre>' . print_r($parser->getErrors(), true) . '</pre>';
-
-
-  foreach ($gedcom->getIndi() as $indi) {
-    echo "<br>ID: " . $indi->id;
-    echo "<br>Sex: " . $indi->sex;
-    if ($indi->name[0]->givn) {
-      echo "<br>Given Names: " . $indi->name[0]->givn;
-    }
-    if ($indi->name[0]->surn) {
-      echo "<br>Surname: " . $indi->name[0]->surn;
-    }
-    if (($indi->name[0]->givn == NULL) && ($indi->name[0]->surn == NULL)) {
-      echo "<br>Full Name: " . $indi->name[0]->name;
-    }
-    echo '<br><br>';
-    foreach ($indi->even as $event) {
-      if ($event->type == 'BIRT') {
-        if ($event->date != NULL) {
-          echo "Birthdate: " . $event->date . "<br>";
-        }
-        if ($event->plac != NULL) {
-          echo "Birthplace " . $event->plac->plac . "<br>";
-        }
-      }
-      if ($event->type == 'DEAT') {
-        if ($event->date != NULL) {
-          echo "Deathdate: " . $event->date . "<br>";
-        }
-        if ($event->plac != NULL) {
-          echo "Deathplace: " . $event->plac->plac . "<br>";
-        }
-      }
-    }
-    foreach ($indi->famc as $family) {
-      echo "Family Child ID: " . $family->famc . "<br>";
-      echo "Parent ID: " . $gedcom->getFam()[$family->famc]->wife . "<br>";
-      echo "Parent ID " . $gedcom->getFam()[$family->famc]->husb . "<br>";
-    }
-    foreach ($indi->fams as $family) {
-      echo "Family Spouse ID: " . $family->fams . "<br>";
-      if ($indi->sex == 'M') {
-        echo "Spouse ID: " . $gedcom->getFam()[$family->fams]->wife . "<br>";
-      } else if ($indi->sex == 'F') {
-        echo "Spouse ID: " . $gedcom->getFam()[$family->fams]->husb . "<br>";
-      }
-      foreach ($gedcom->getFam()[$family->fams]->chil as $child) {
-        echo "Child ID: " . $child . "<br>";
-      }
-    }
-    echo '<br><br>';
-  }
 }
