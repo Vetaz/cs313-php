3e6192f34b8b3f1081ea25756aa4db14eb4870b1
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-02-16 19:20:14 -0700

    changed header

--- web/Relationships/get-relationship.php
+++ web/Relationships/get-relationship.php
@@ -15,13 +15,13 @@
   <header>
     <h1>Get the Relationship between Two People in Your Gedcom</h1>
     <nav>
-      <a href="get-relationship.php" class="button">Get Relationships</a>
-      <a href="index.html" class="button">Home</a>
+    <a href="uploadGedcom.php" class="button">Upload Your Gedcom</a>
+        <a href="get-relationship.php" class="button">Get Relationships</a>
     </nav>
   </header>
   <main>
     <div>
-      <p> Hello <?php $username ?>!</p>
+      <p> Hello <?php $_SESSION['username'] ?>!</p>
       <p>Type the ID number of each person for who you would like to see the relationship for.</p>
       <form method="get" action="relationship-viewer.php">
         <p>Type ID of person 1:</p>
