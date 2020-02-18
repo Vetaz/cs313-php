2b8805f398ede5e89a194c9be3bc1a7f17864d3b
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-02-17 23:27:48 -0700

    added correct gedcom id variable for privatize

--- web/Relationships/privatizing.php
+++ web/Relationships/privatizing.php
@@ -5,10 +5,9 @@ $db = get_db();
 
 if (isset($_POST['gedcom_id'])) {
   $gedcom_id = htmlspecialchars($_POST['gedcom_id']);
+
+  $privatize = $db->prepare("UPDATE person SET name = 'Living', birthdate = '', birthplace = '' WHERE gedcom_id = $gedcom_id AND deathdate = '' AND deathplace = ''");
+  $privatize->execute();
 }
-
-$privatize = $db->prepare("UPDATE person SET name = 'Living', birthdate = '', birthplace = '' WHERE gedcom_id = 29 AND deathdate = '' AND deathplace = ''");
-$privatize->execute();
-
 header('Location: get-relationship.php');
 ?>
