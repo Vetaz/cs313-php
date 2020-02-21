ef31e84bbec12c7594e9a59befc35bd8098aca72
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-02-21 13:07:49 -0700

    added spouse, parent, and child relationships

--- web/Relationships/uploadDatabase.php
+++ web/Relationships/uploadDatabase.php
@@ -87,7 +87,7 @@
     }
 
     $query = "INSERT INTO person (gedcom_id, id, name, sex, birthdate, birthplace, deathdate, deathplace) VALUES ('$gedcom_id', '$id', '$name', '$sex', '$birthDate', '$birthPlace', '$deathDate','$deathPlace')";
-/*
+
     echo "<p>Individual $id ";
     if ($name) {
       echo "Name: $name ";
@@ -106,21 +106,39 @@
       echo "Death place: $deathPlace ";
     }
     echo "</p>";
-*/
+
     $insertPerson = $db->prepare($query);
     $insertPerson->execute();
+  
     foreach ($indi->famc as $family) {
-      $parentId[] = $gedcom->getFam()[$family->famc]->wife;
-      $parentId[] = $gedcom->getFam()[$family->famc]->husb;
+      $parentId = substr($gedcom->getFam()[$family->famc]->wife, 1);
+      $query = "INSERT INTO person_parent (gedcom_id, person_id, parent_id) VALUES ('$gedcom_id', '$id', '$parentId')";
+      $insertParent = $db->prepare($query);
+      $insertParent->execute();
+      echo $query . "<br>";
+
+      $parentId = substr($gedcom->getFam()[$family->famc]->husb, 1);
+      $insertParent = $db->prepare($query);
+      $insertParent->execute();
+      echo $query . "<br>";
     }
     foreach ($indi->fams as $family) {
       if ($indi->sex == 'M') {
-        $spouse = $gedcom->getFam()[$family->fams]->wife;
+        $spouseId = substr($gedcom->getFam()[$family->fams]->wife, 1);
       } else if ($indi->sex == 'F') {
-        $spouse = $gedcom->getFam()[$family->fams]->husb;
+        $spouseId = substr($gedcom->getFam()[$family->fams]->husb, 1);
       }
-      foreach ($gedcom->getFam()[$family->fams]->chil as $child) {
-        $child;
+      $query = "INSERT INTO person_spouse (gedcom_id, person_id, spouse_id) VALUES ('$gedcom_id', '$id', '$spouseId')";
+      $insertSpouse = $db->prepare($query);
+      $insertSpouse->execute();
+      echo $query . "<br>";
+
+      foreach ($gedcom->getFam()[$family->fams]->chil as $childId) {
+        $childId = substr($childId, 1);
+        $query = "INSERT INTO person_child (gedcom_id, person_id, child_id) VALUES ('$gedcom_id', '$id', '$childId')";
+        $insertSpouse = $db->prepare($query);
+        $insertSpouse->execute();
+        echo $query . "<br>";
       }
     }
   } 
